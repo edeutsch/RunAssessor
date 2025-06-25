fa7f175449e65cd2b96852f70a88e1b3e50f9b0e
Eric Deutsch <ericdeutsch@gmail.com>
2025-06-25 08:29:43 -0700

    add an initial set of tests

---
+++ tests/test_metadata_handler.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.append("../lib")
+from metadata_handler import MetadataHandler
+
+
+def test_init_with_various_parameters():
+    metadata = MetadataHandler()
+    assert metadata.metadata_filepath.endswith('.json')
+    metadata = MetadataHandler('')
+    assert metadata.metadata_filepath.endswith('.json')
+    metadata = MetadataHandler('.')
+    assert metadata.metadata_filepath.endswith('.json')
+    assert len(metadata.sdrf_hints) == 0
+
+
+def test_create_template():
+    metadata = MetadataHandler()
+    result = metadata.create_template()
+    assert len(metadata.metadata) == 6
+    assert len(metadata.metadata['problems']['errors']) == 3
+    assert metadata.metadata['problems']['errors']['count'] == 0
+
+
