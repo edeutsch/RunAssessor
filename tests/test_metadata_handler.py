91c2cb140ff36c338f1d3afbac71ff4648b1df1b
Eric Deutsch <ericdeutsch@gmail.com>
2026-01-27 10:41:16 -0800

    Tidy up test based on new logic that a template .txt file is always created

--- tests/test_metadata_handler.py
+++ tests/test_metadata_handler.py
@@ -30,42 +30,35 @@ def test_create_template():
     assert metadata.metadata['problems']['errors']['count'] == 0
 
 
-#Unit test to check that function will use template file if study_metadata.txt is not availible
-def test_find_txt_file(capsys):
+# Unit tests to check that study_metadata.txt will be created based on template if not available
+def test_find_txt_file():
 
-    ### Creates study_metadata.txt
     filename = "study_metadata.txt"
     file_path = os.path.join(os.getcwd(), filename)
+
+    ### Create an empty study_metadata.txt
     open(file_path, 'w').close()
 
-    #### study_metadata.txt file exists
+    #### Verify that function finds it
     metadata = MetadataHandler("study_metadata.json", verbose=2)
     file = metadata.find_txt_file()
     assert file == "study_metadata.txt"    
 
-    #### study_metadata.txt file exists 
-    metadata = MetadataHandler("study_metadata.json", verbose=2)
-    file = metadata.find_txt_file()
-    assert file == "study_metadata.txt" 
-    
-    #### defualt study_metadata.json is used and no stud_metadat.txt ecists so base template should be used
-    #### study_metadata.txt should be generated
+    #### Test that a study_metadata.txt is generated from the template
     os.remove(file_path)
     metadata = MetadataHandler(verbose=2)
     file = metadata.find_txt_file()
 
-    if os.path.exists(os.getcwd() + "/study_metadata.txt"):
-        os.remove(os.getcwd() + "/study_metadata.txt")
+    if os.path.exists(file_path):
+        os.remove(file_path)
     else:   
-        raise AssertionError(f"File {file_path} was not generated:" + 
-                             os.getcwd() + "/study_metadata.txt")
+        raise AssertionError(f"File {file_path} was not generated")
     
-    #### study_metadata.txt is specified by user but does not exits, SDRF table wont be made
-    metadata = MetadataHandler("study_metadata.json", verbose=2)
-    file = metadata.find_txt_file()
-    assert file == None
-
-    #### Should not find baloon.txt and not generate the sdrf table
+    #### Verify that a balloon.txt is created to accompany the balloon.json file
+    filename = "balloon.txt"
+    file_path = os.path.join(os.getcwd(), filename)
     metadata = MetadataHandler("balloon.json", verbose=2)
     file = metadata.find_txt_file()
-    assert file == None
+    assert file == filename
+    os.remove(file_path)
+
